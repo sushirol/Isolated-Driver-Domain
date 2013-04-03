2a8c89761b3b2aae6942c41a40c024d34fe2a0a7
root <sushrut@vt.edu>
2013-04-03 10:54:53 -0400

    frontend working.

--- myrepo/frontend/frontend.h
+++ myrepo/frontend/frontend.h
@@ -34,14 +34,22 @@
 #define IDD_SYSID_CURRENT   IDD_SYSID_ONE
 
 
+#define IDD_MAX_SEGMENTS_PER_REQUEST 11
 struct idd_request {
 	int data_direction;
-	unsigned long nbytes;
-	unsigned long offset;
-	unsigned long seq_no;
-	struct work_struct work;
+//	unsigned long nbytes;
+//	unsigned long offset;
+	uint8_t nr_segments;
+	uint64_t sector_number;
+	struct idd_request_segment {
+		grant_ref_t gref;	/* reference to I/O buffer frame */
+		/* @first_sect: first sector in frame to transfer (inclusive).   */
+		/* @last_sect: last sector in frame to transfer (inclusive).     */
+		uint8_t     first_sect, last_sect;
+	}seg[IDD_MAX_SEGMENTS_PER_REQUEST];
+	uint64_t seq_no;
 	void *priv_data;
-};
+}__attribute__((__packed__));
 
 struct idd_response {
 	int res;
@@ -52,38 +60,39 @@ struct idd_response {
 
 DEFINE_RING_TYPES(idd, struct idd_request, struct idd_response);
 
-#define BLK_RING_SIZE __CONST_RING_SIZE(idd, PAGE_SIZE)
-  
+#define IDD_RING_SIZE __CONST_RING_SIZE(idd, PAGE_SIZE)
+#define IDD_BOUNCE_ANY          (-1ULL)
+ 
+struct idd_shadow {
+	struct idd_request req;
+	struct request *request;
+	unsigned long frame[IDD_MAX_SEGMENTS_PER_REQUEST];
+	struct grant *grants_used[IDD_MAX_SEGMENTS_PER_REQUEST];
+}; 
+ 
 typedef struct idd_irq_info {
 	struct idd_front_ring main_ring;
 	struct idd_front_ring data_ring;
-//        idd_shared_data_t *main;
-//        struct vm_struct *main_area;
 	struct vm_struct *main_ring_area;
 	struct vm_struct *data_ring_area;
-//	grant_handle_t main_handle;
  	grant_handle_t main_ring_handle;
 	grant_handle_t data_ring_handle;
 //	int main_irq;
 	int ring_irq;
 	uint32_t domid;
-	atomic_t counter;
-	atomic_t notify_counter;
-	int request_avail;
-	uint32_t state;
-	uint32_t gref;
-	uint32_t expand_tgid;
-	uint32_t expand_gref;
-	wait_queue_head_t queue;
-	wait_queue_head_t request_queue;
-	struct semaphore state_sem;
 	struct semaphore rw_sem;
 	struct semaphore notify_sem;
-	struct spinlock notify_lock;
-	struct task_struct *request_thread;
-	void * io_data_page;
+//	void * io_data_page;
+	struct scatterlist sg[IDD_MAX_SEGMENTS_PER_REQUEST];
 	unsigned long id;
 	struct request_queue *rq;
+	struct llist_head persistent_gnts;
+	unsigned int persistent_gnts_c;
+	struct idd_shadow shadow[IDD_RING_SIZE];
+	unsigned long shadow_free;
+	struct gnttab_free_callback callback;
+	struct work_struct work;
+	spinlock_t io_lock;
 } idd_irq_info_t;
 
 
