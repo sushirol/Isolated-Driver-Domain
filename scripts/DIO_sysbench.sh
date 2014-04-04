1c2ed0afafcd426f5752957bb96c9a6858efc109
root <root@localhost.localdomain>
2014-04-04 15:51:04 +0000

    16 32 threads

--- scripts/DIO_sysbench.sh
+++ scripts/DIO_sysbench.sh
@@ -1,11 +1,14 @@
 filename="results.txt"
 touch $filename
-for i in 1 2 3 4 5 6 7 8 9 10
+req=0
+for i in 1 2 3 4 5 6 7 8 9 10 16 24 32 
 do
-    echo "sysbench --num-threads=$i --test=fileio --file-total-size=1G --max-requests=100000 --file-extra-flags=direct --file-test-mode=$1 run" >> $filename
+  req=$(expr $i \* 10000 )
+  echo $req
+    echo "sysbench --num-threads=$i --test=fileio --file-total-size=1G --max-requests=$req --file-extra-flags=direct --file-test-mode=$1 run" >> $filename
     echo "
     " >> $filename
-    sysbench --num-threads=$i --test=fileio --file-total-size=1G --max-requests=100000 --file-extra-flags=direct --file-test-mode=$1 run >> $filename
+    sysbench --num-threads=$i --test=fileio --file-total-size=1G --max-requests=$req --file-extra-flags=direct --file-test-mode=$1 run >> $filename
     echo "=======================================================
     " >> $filename
 done
