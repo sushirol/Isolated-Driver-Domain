a18d3b8c8eb6184cda2df5f1e88b1e1ce1a0b56d
sushrut <sushrut@vt.edu>
2014-04-03 20:56:40 -0400

    removed debug msgs

--- scripts/calc.py
+++ scripts/calc.py
@@ -4,12 +4,12 @@ fn = sys.argv[1]
 filenameList = []
 outputList = []
 for i in range(1, 11):
-    print i
+#    print i
     filenameList.append(fn + '-' + `i` + '.txt')
     outputList.append('cpu-' + fn + '-' + `i` + '.cpu')
 
-print filenameList
-print outputList
+#print filenameList
+#print outputList
 
 for i in range (0,10):
     stat_fd = open(filenameList[i],"r")
