e00f32233c404a33ef53dcac0a4364fa8b3817d2
root <root@localhost.localdomain>
2014-04-04 19:05:22 +0000

    for new threads

--- scripts/DIO_sysbench.sh
+++ scripts/DIO_sysbench.sh
@@ -1,16 +1,16 @@
 filename="results.txt"
 cpufile="cpu.txt"
-touch $cpuname
+touch $cpufile
 touch $filename
 req=0
 for i in 1 2 3 4 5 6 7 8 9 10 16 24 32 
 do
-  req=$(expr $i \* 100000 )
+  req=$(expr $i \* 10000 )
   echo "sysbench --num-threads=$i --test=fileio --file-total-size=1G --max-requests=$req --file-extra-flags=direct --file-test-mode=$1 run" >> $filename
   echo -e "\n" >> $filename
-  echo "thread $i" > $cpufile
-  cat /proc/stat | grep "cpu " > $cpufile
+  echo "thread $i" >> $cpufile
+  cat /proc/stat | grep "cpu " >> $cpufile
   sysbench --num-threads=$i --test=fileio --file-total-size=1G --max-requests=$req --file-extra-flags=direct --file-test-mode=$1 run >> $filename
-  cat /proc/stat | grep "cpu " > $cpufile
+  cat /proc/stat | grep "cpu " >> $cpufile
   echo -e "=======================================================\n" >> $filename
 done
--- scripts/c-average.py
+++ scripts/c-average.py
@@ -8,21 +8,21 @@ fList=[]
 a = []
 
 for i in range(1, 11):
-    filenameList.append(filename + '-' + `i` + '.cpu')
+    filenameList.append(filename + '-' + `i` + '.cal')
 
 for i in range(0, 10):
     fList.append(open(filenameList[i],"r"))
 
 f_av = open(filename + '-av.data',"w")
 
-for j in range(0,10):
+for j in range(0,13):
     temp = 0
     for i in range(0, 10):
         temp = float(fList[i].readline()) + temp
     temp = temp/10
     a.append(temp)
 
-for i in range(0, 10):
+for i in range(0, 13):
     f_av.write(`i+1`)
     f_av.write(" ")
     f_av.write(`a[i]`)
