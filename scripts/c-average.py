8ed6d31f3e1422930992563e8f2460e3a2c4d9a2
sushrut <sushrut@vt.edu>
2014-04-03 20:57:57 -0400

    new calc average file

---
+++ scripts/c-average.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import sys
+filename = sys.argv[1]
+
+filenameList = []
+outputList = []
+fList=[]
+a = []
+
+for i in range(1, 11):
+    filenameList.append(filename + '-' + `i` + '.cpu')
+
+for i in range(0, 10):
+    fList.append(open(filenameList[i],"r"))
+
+f_av = open(filename + '-av.txt',"w")
+
+for j in range(0,10):
+    temp = 0
+    for i in range(0, 10):
+        temp = float(fList[i].readline()) + temp
+    temp = temp/10
+    a.append(temp)
+
+for i in range(0, 10):
+    f_av.write(`a[i]`)
+    f_av.write("\n")
+    print a[i]
+
+for i in range(0, 10):
+    fList[i].close()
+f_av.close()
